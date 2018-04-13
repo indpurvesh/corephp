ee23ed0bda2c7659a8bab9edb3e9458b24fd4d8e
indpurvesh <ind.purvesh@gmail.com>
2018-04-14 10:32:21 +1200

    corephp initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require __DIR__.'/vendor/autoload.php';
+
+$app = require_once __DIR__.'/bootstrap/app.php';
+
+$app->run();
---
+++ lib/Foundation/App.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AvoRed\Foundation;
+
+class App {
+    public function run() {
+        echo "Hello World";
+    }
+}
